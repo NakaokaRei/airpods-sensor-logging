40e2f1c831b0801467a0bdb849b37f41434e7801
rei.nakaoka <reideeplearning@gmail.com>
2022-05-27 17:21:51 +0900

    first commit

---
+++ airpods-sensor-logging/airpods_sensor_loggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  airpods_sensor_loggingApp.swift
+//  airpods-sensor-logging
+//
+//  Created by rei.nakaoka on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct airpods_sensor_loggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
